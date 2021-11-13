0da0bf70e3b5e8cf668e85dc12cb76dbea3b533c
polyqubit <polyumbrial@gmail.com>
2021-11-13 12:15:26 -0800

    Update Mountain.java

--- Mountain/Mountain.java
+++ Mountain/Mountain.java
@@ -1,3 +1,16 @@
 public class Mountain {
-
+	
+	public static boolean isIncreasing(int[] array, int stop) {
+		
+	}
+	public static boolean isDecreasing(int[] array, int start) {
+		
+	}
+	public static int getPeakIndex(int[] array) {
+		
+	}
+	public static boolean isMountain(int[] array) {
+		
+	}
+	
 }
